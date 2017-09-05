969fe658b55d7ef674f5ad7fe7dab24e92e50d35
Raniery Ribeiro <raniery.rrr@gmail.com>
2017-09-05 00:53:25 -0300

    Implementação inicial do POST

---
+++ src/SystemBundle/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SystemBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username')->add('password');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => \SystemBundle\DTO\User::class,
+            'csrf_protection'   => false,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'systembundle_user';
+    }
+
+
+}
